9b2ac59adf29a2b55aec202bfe7c3b23ee477c71
injahow <i@injahow.com>
2020-10-17 17:48:36 +0800

    style: set 'tindent_size=2'

--- tools/bilibili-parse-download.user.js
+++ tools/bilibili-parse-download.user.js
@@ -10,72 +10,72 @@
 // @require      https://static.hdslb.com/js/jquery.min.js
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
-  let aid = '', p = '', q='';
+  let aid = '', p = '', q = '';
   let aid_temp = '', p_temp = '', q_temp = '';
 
   let topBox =
-      "<div style='position:fixed;z-index:999999;cursor:pointer;top:60px;left:0px;'>"+
-        "<div id='bilibili_parse' style='font-size:14px;padding:10px 2px;color:#000000;background-color:#00a1d6;'>请求地址</div>"+
-        "<div style='font-size:14px;padding:10px 2px;'>"+
-          "<a id='video_url' style='display:none' target='_blank' referrerpolicy='origin' href='#'>下载视频</a>"+
-        "</div>"+
-      "</div>";
+    "<div style='position:fixed;z-index:999999;cursor:pointer;top:60px;left:0px;'>" +
+      "<div id='bilibili_parse' style='font-size:14px;padding:10px 2px;color:#000000;background-color:#00a1d6;'>请求地址</div>" +
+        "<div style='font-size:14px;padding:10px 2px;'>" +
+        "<a id='video_url' style='display:none' target='_blank' referrerpolicy='origin' href='#'>下载视频</a>" +
+      "</div>" +
+    "</div>";
   $('body').append(topBox)
 
   //获取视频编号参数aid
   const link_av = $('link:first')[0].href
   const patt = /bilibili.com\/video\/av\d+/g
-  if(patt.test(link_av)){
-      aid = link_av.replace(/[^0-9]/ig, '')
-      console.log('获取aid:',aid)
+  if (patt.test(link_av)) {
+    aid = link_av.replace(/[^0-9]/ig, '')
+    console.log('获取aid:', aid)
   } else {
-      console.log('aid获取出错！')
+    console.log('aid获取出错！')
   }
 
   // todo:获取视频分辨率参数q
   q = q || '64'
 
-  $('body').on('click','#bilibili_parse',function(){
-      // aid错误
-      if (aid === '') return
+  $('body').on('click', '#bilibili_parse', function () {
+    // aid错误
+    if (aid === '') return
 
-      //获取视频分页参数p
-      let query_arr = window.location.search.substring(1).split('&');
-      for (let i=0; i<query_arr.length; i++) {
-          let pair = query_arr[i].split('=')
-          if(pair[0] == 'p'){
-              p = pair[1]
-          }
+    //获取视频分页参数p
+    let query_arr = window.location.search.substring(1).split('&');
+    for (let i = 0; i < query_arr.length; i++) {
+      let pair = query_arr[i].split('=')
+      if (pair[0] == 'p') {
+        p = pair[1]
       }
-      p = p || '1'
+    }
+    p = p || '1'
 
-      if (aid === aid_temp && p === p_temp && q === q_temp){
-          console.log('请勿重复请求')
-          return
+    if (aid === aid_temp && p === p_temp && q === q_temp) {
+      console.log('请勿重复请求')
+      return
+    }
+    aid_temp = aid
+    p_temp = p
+    q_temp = q
+
+    console.log('开始解析')
+    $.ajax({
+      url: `https://api.injahow.cn/bparse/?av=${aid}&p=${p}&q=${q}&otype=url`,
+      dataType: 'text',
+      success: function (result) {
+        console.log('url获取成功', result)
+        $('#video_url').attr('href', result)
+        $('#video_url').show()
       }
-      aid_temp = aid
-      p_temp = p
-      q_temp = q
-
-      console.log('开始解析')
-      $.ajax({
-          url:`https://api.injahow.cn/bparse/?av=${aid}&p=${p}&q=${q}&otype=url`,
-          dataType:'text',
-          success:function(result){
-              console.log('url获取成功',result)
-              $('#video_url').attr('href', result)
-              $('#video_url').show()
-          }
-      })
+    })
 
   })
 
   // 监听p参数变化
-  $('body').on('click','.list-box',function(){
-      $('#video_url').hide()
+  $('body').on('click', '.list-box', function () {
+    $('#video_url').hide()
   })
 
 })();
