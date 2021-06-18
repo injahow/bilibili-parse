59a30fe56af3049485225d0737db4c1997944e4d
injahow <i@injahow.com>
2021-06-18 13:05:21 +0800

    Update bilibili-parse-download.user.js

--- tools/bilibili-parse-download.user.js
+++ tools/bilibili-parse-download.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         bilibili视频下载
-// @version      0.4.4
+// @version      0.4.5
 // @description  支持下载番剧与用户上传视频，自动切换为高清视频源
 // @author       injahow
 // @copyright    2021, injahow (https://github.com/injahow)
@@ -21,14 +21,121 @@
 (function() {
     'use strict';
 
-    let aid = '', epid = '', p = '', q = '', cid = '';
+    let aid = '', p = '', q = '', cid = '', epid = '';
     let aid_temp = '', p_temp = '', q_temp = '';
-    let need_vip = false, is_login = false, vip_status = 0;
-    let my_toolbar = '', player_tag = '';
-    let flag_name = '';
+    let flag_name = '', need_vip = false, vip_need_pay = false;
+    let is_login = false, vip_status = 0;
+
+    function request_danmaku(options, _cid){
+        $.ajax({
+            url: `https://api.bilibili.com/x/v1/dm/list.so?oid=${_cid}`,
+            dataType: 'text',
+            success:function(result){
+                const result_dom = $(result.replace(/[\x00-\x08\x0b-\x0c\x0e-\x1f\x7f]/g, ''));
+                if(!result_dom.find('d')[0]){
+                    options.error('弹幕请求失败');
+                }else{
+                    const danmaku_data = result_dom.find('d').map((i, el) => {
+                        const item = $(el);
+                        const p = item.attr('p').split(',');
+                        let type = 0;
+                        if(p[1] === '4'){
+                            type = 2;
+                        }else if(p[1] === '5'){
+                            type = 1;
+                        }
+                        return [{author: '', time: parseFloat(p[0]), type: type, color: parseInt(p[3]), id: '', text: item.text()}];
+                    }).get();
+                    options.success(danmaku_data);
+                }
+            },
+            error:function(error){
+                options.error('弹幕请求异常');
+            }
+        });
+    }
+
+    function replace_player(url){
+        if(!!$('#bilibiliPlayer')[0]){
+            $('#bilibiliPlayer').before('<div id="my_dplayer" class="bilibili-player relative bilibili-player-no-cursor"></div>');
+            $('#bilibiliPlayer').hide();
+        }else{
+            $('#bilibili-player').html('<div id="my_dplayer" class="bilibili-player relative bilibili-player-no-cursor" style="width:100%;height:100%;"></div>');
+        }
+        $('#danmukuBox').hide();//隐藏弹幕列表
+        !!$('#player_mask_module')[0] && $('#player_mask_module').hide();
+        window.my_dplayer = new DPlayer({
+            container: $('#my_dplayer')[0],
+            video: {
+                url: url,
+                type: 'auto'
+            },
+            danmaku: true,
+            apiBackend: {
+                read: function (options) {
+                    request_danmaku(options, cid);
+                },
+                send: function (options) {
+                    options.error('此脚本无法发送弹幕');
+                }
+            }
+        });
+    }
+
+    function get_video_status(){
+        let location_href = window.location.href;
+        if(location_href.match(/bilibili.com\/bangumi\/play\/ep/)){
+            flag_name = 'ep';
+            need_vip = window.__INITIAL_STATE__.epInfo.badge === '会员';
+            vip_need_pay = window.__INITIAL_STATE__.epPayMent.vipNeedPay;
+        }else if(location_href.match(/bilibili.com\/bangumi\/play\/ss/)){
+            flag_name = 'ss';
+            need_vip = window.__INITIAL_STATE__.epInfo.badge === '会员';
+            vip_need_pay = window.__INITIAL_STATE__.epPayMent.vipNeedPay;
+        }else if(location_href.match(/bilibili.com\/video\/av/)){
+            flag_name = 'av';
+            need_vip = false;
+            vip_need_pay = false;
+        }else if(location_href.match(/bilibili.com\/video\/BV/)){
+            flag_name = 'bv';
+            need_vip = false;
+            vip_need_pay = false;
+        }
+    }
+
+    function get_all_id(){
+        let _aid, _cid;
+        if(flag_name === 'ep' || flag_name === 'ss'){
+            _aid = window.__INITIAL_STATE__.epInfo.aid;
+            _cid = window.__INITIAL_STATE__.epInfo.cid;
+        }else if(flag_name === 'av' || flag_name === 'bv') {
+            _aid = window.__INITIAL_STATE__.videoData.aid;
+            _cid = window.__INITIAL_STATE__.videoData.cid;
+        }
+        return {aid: _aid, cid: _cid}
+    }
+
+    function refresh(){
+        console.log('refresh...');
+        !!('#video_download')[0] && $('#video_download').hide();
+        if (window.my_dplayer){
+            console.log('销毁dplayer');
+            window.my_dplayer.destroy();
+            window.my_dplayer = null;
+            $('#my_dplayer').remove();
+            !!$('#bilibiliPlayer')[0] && $('#bilibiliPlayer').show();
+            !$('#bilibiliPlayer')[0] && $('#player_mask_module').show();
+        }
+        // 更新cid和aid - 1
+        const ids = get_all_id();
+        aid = ids.aid;
+        cid = ids.cid;
+    }
+
     $('body').append('<a id="video_url" style="display:none" target="_blank" referrerpolicy="origin" href="#"></a>');
     // 暂且延迟处理...
     setTimeout(function(){
+        let my_toolbar;
         if(!!$('#arc_toolbar_report')[0]){
             my_toolbar =
                 '<div id="arc_toolbar_report_2" class="video-toolbar report-wrap-module report-scroll-module" scrollshow="true"><div class="ops">'+
@@ -49,76 +156,6 @@
     $('body').on('click', '#video_download',function(){
         $('#video_url')[0].click();
     });
-
-    function replace_player(url){
-        player_tag = '#bilibiliPlayer';
-        if(!!$('#bilibiliPlayer')[0]){
-            $('#bilibiliPlayer').before('<div id="my_dplayer" class="bilibili-player relative bilibili-player-no-cursor"></div>');
-            $('#bilibiliPlayer').hide();
-        }else{
-            $('#bilibili-player').html('<div id="my_dplayer" class="bilibili-player relative bilibili-player-no-cursor" style="width:100%;height:100%;"></div>');
-        }
-        $('#danmukuBox').hide();//隐藏弹幕列表
-        !!$('#player_mask_module')[0] && $('#player_mask_module').hide();
-        window.my_dplayer = new DPlayer({
-            container: $('#my_dplayer')[0],
-            video: {
-                url: url,
-                type: 'auto'
-            },
-            danmaku: true,
-            apiBackend: {
-                read: function (options) {
-                    $.ajax({
-                        url: `https://api.bilibili.com/x/v1/dm/list.so?oid=${cid}`,
-                        dataType: 'text',
-                        success:function(result){
-                            const result_dom = $(result.replace(/[\x00-\x08\x0b-\x0c\x0e-\x1f\x7f]/g, ''));
-                            if(!result_dom.find('d')[0]){
-                                options.error('弹幕请求失败');
-                            }else{
-                                const danmaku_data = result_dom.find('d').map((i, el) => {
-                                    const item = $(el);
-                                    const p = item.attr('p').split(',');
-                                    let type = 0;
-                                    if(p[1] === '4'){
-                                        type = 2;
-                                    }else if(p[1] === '5'){
-                                        type = 1;
-                                    }
-                                    return [{author: '', time: parseFloat(p[0]), type: type, color: parseInt(p[3]), id: '', text: item.text()}];
-                                }).get();
-                                options.success(danmaku_data);
-                            }
-                        },
-                        error:function(error){
-                            options.error('弹幕请求异常');
-                        }
-                    });
-                },
-                send: function (options) {
-                    options.error('此脚本无法发送弹幕')
-                }
-            }
-        });
-    }
-
-    function get_video_status(){
-        let location_href = window.location.href;
-        if(location_href.match(/bilibili.com\/bangumi\/play\/ep/)){
-            flag_name = 'ep';
-            need_vip = window.__INITIAL_STATE__.epInfo.badge === '会员';
-        }else if(location_href.match(/bilibili.com\/bangumi\/play\/ss/)){
-            flag_name = 'ss';
-            need_vip = window.__INITIAL_STATE__.epInfo.badge === '会员';
-        }else if(location_href.match(/bilibili.com\/video\/av/)){
-            flag_name = 'av';
-            need_vip = false;
-        }else if(location_href.match(/bilibili.com\/video\/BV/)){
-            flag_name = 'bv';
-            need_vip = false;
-        }
-    }
 
     $('body').on('click', '#bilibili_parse',function(){
         get_video_status();
@@ -220,43 +257,18 @@
         });
     });
 
-    function get_all_id(){
-        let _aid, _cid;
-        if(flag_name === 'ep' || flag_name === 'ss'){
-            _aid = window.__INITIAL_STATE__.epInfo.aid;
-            _cid = window.__INITIAL_STATE__.epInfo.cid;
-        }else if(flag_name === 'av' || flag_name === 'bv') {
-            _aid = window.__INITIAL_STATE__.videoData.aid;
-            _cid = window.__INITIAL_STATE__.videoData.cid;
-        }
-        return {aid: _aid, cid: _cid}
-    }
-
-    function refresh(){
-        console.log('refresh...');
-        !!('#video_download')[0] && $('#video_download').hide();
-        if (window.my_dplayer){
-            console.log('销毁dplayer');
-            window.my_dplayer.destroy();
-            window.my_dplayer = null;
-            $('#my_dplayer').remove();
-            !!$('#bilibiliPlayer')[0] && $('#bilibiliPlayer').show();
-            get_video_status();// 更新视频状态
-            vip_status === 0 && need_vip && !!$('#player_mask_module')[0] && $('#player_mask_module').show();
-        }
-        // 更新cid和aid - 1
-        const ids = get_all_id();
-        aid = ids.aid;
-        cid = ids.cid;
-    }
-
     // 监听p
-    $('body').on('click', '.list-box', function(){
-        refresh();
+    $('body').on('click', 'a.router-link-active', function(){
+        if(this!==$('li[class="on"]').find('a')[0]){
+            refresh();
+        }
     });
 
     $('body').on('click', 'li.ep-item', function(){
-        refresh();
+        if(!$(this).find('.cursor')){
+            refresh();
+        }
+
     });
 
     // 监听q
