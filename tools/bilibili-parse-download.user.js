592e942fb824b60e12fc6e0415bdec78366badbd
injahow <i@injahow.com>
2021-06-19 00:35:04 +0800

    Update bilibili-parse-download.user.js

--- tools/bilibili-parse-download.user.js
+++ tools/bilibili-parse-download.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         bilibili视频下载
-// @version      0.4.5
+// @version      0.5.0
 // @description  支持下载番剧与用户上传视频，自动切换为高清视频源
 // @author       injahow
 // @copyright    2021, injahow (https://github.com/injahow)
@@ -18,54 +18,55 @@
 // @require      https://cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.js
 // ==/UserScript==
 /* globals $, DPlayer waitForKeyElements */
-(function() {
+(function () {
     'use strict';
-
+    const USE_DASH = false; // 使用DASH视频源（音视频分离，拖拽进度条不会卡死，下载可能失败）
     let aid = '', p = '', q = '', cid = '', epid = '';
     let aid_temp = '', p_temp = '', q_temp = '';
     let flag_name = '', need_vip = false, vip_need_pay = false;
     let is_login = false, vip_status = 0;
-
-    function request_danmaku(options, _cid){
+    function request_danmaku(options, _cid) {
         $.ajax({
             url: `https://api.bilibili.com/x/v1/dm/list.so?oid=${_cid}`,
             dataType: 'text',
-            success:function(result){
+            success: function (result) {
                 const result_dom = $(result.replace(/[\x00-\x08\x0b-\x0c\x0e-\x1f\x7f]/g, ''));
-                if(!result_dom.find('d')[0]){
+                if (!result_dom.find('d')[0]) {
                     options.error('弹幕请求失败');
-                }else{
+                } else {
                     const danmaku_data = result_dom.find('d').map((i, el) => {
                         const item = $(el);
                         const p = item.attr('p').split(',');
                         let type = 0;
-                        if(p[1] === '4'){
+                        if (p[1] === '4') {
                             type = 2;
-                        }else if(p[1] === '5'){
+                        } else if (p[1] === '5') {
                             type = 1;
                         }
-                        return [{author: '', time: parseFloat(p[0]), type: type, color: parseInt(p[3]), id: '', text: item.text()}];
+                        return [{ author: '', time: parseFloat(p[0]), type: type, color: parseInt(p[3]), id: '', text: item.text() }];
                     }).get();
                     options.success(danmaku_data);
                 }
             },
-            error:function(error){
+            error: function (error) {
                 options.error('弹幕请求异常');
             }
         });
     }
 
-    function replace_player(url){
-        if(!!$('#bilibiliPlayer')[0]){
-            $('#bilibiliPlayer').before('<div id="my_dplayer" class="bilibili-player relative bilibili-player-no-cursor"></div>');
+    function replace_player(url, url_2) {
+        if (!!$('#bilibiliPlayer')[0]) {
+            $('#bilibiliPlayer').before('<div id="my_dplayer" class="bilibili-player relative bilibili-player-no-cursor">');
             $('#bilibiliPlayer').hide();
-        }else{
+        } else {
             $('#bilibili-player').html('<div id="my_dplayer" class="bilibili-player relative bilibili-player-no-cursor" style="width:100%;height:100%;"></div>');
         }
+        USE_DASH && $('#bilibiliPlayer').before(`</div><div id="my_dplayer_2" download="${cid+'-'+q}.mp4" style="display:none"></div>`);
         $('#danmukuBox').hide();//隐藏弹幕列表
         !!$('#player_mask_module')[0] && $('#player_mask_module').hide();
         window.my_dplayer = new DPlayer({
             container: $('#my_dplayer')[0],
+            mutex: false,
             video: {
                 url: url,
                 type: 'auto'
@@ -76,49 +77,88 @@
                     request_danmaku(options, cid);
                 },
                 send: function (options) {
-                    options.error('此脚本无法发送弹幕');
+                    options.error('此脚本无法将弹幕同步到云端！');
                 }
             }
         });
+        if (USE_DASH && url_2 && url_2 !== '#') {
+            window.my_dplayer_2 = new DPlayer({
+                container: $('#my_dplayer_2')[0],
+                mutex: false,
+                video: {
+                    url: url_2,
+                    type: 'auto'
+                }
+            });
+            const my_dplayer = window.my_dplayer;
+            const my_dplayer_2 = window.my_dplayer_2;
+            my_dplayer.on('play', function () {
+                !my_dplayer.paused && my_dplayer_2.play();
+            });
+            my_dplayer.on('playing', function () {
+                !my_dplayer.paused && my_dplayer_2.play();
+            });
+            my_dplayer.on('timeupdate', function () {
+                my_dplayer_2.paused && my_dplayer_2.pause();
+                !my_dplayer.paused && my_dplayer_2.play();
+                my_dplayer_2.speed(my_dplayer.video.playbackRate);
+            });
+            my_dplayer.on('seeking', function () {
+                my_dplayer_2.pause();
+                my_dplayer_2.seek(my_dplayer.video.currentTime)
+            });
+            my_dplayer.on('waiting', function () {
+                my_dplayer_2.pause();
+            });
+            my_dplayer.on('pause', function () {
+                my_dplayer_2.pause();
+            });
+            my_dplayer.on('suspend', function () {
+                my_dplayer_2.speed(my_dplayer.video.playbackRate);
+            });
+            my_dplayer.on('volumechange', function () {
+                my_dplayer_2.volume(my_dplayer.video.volume);
+            });
+        }
     }
 
-    function get_video_status(){
+    function get_video_status() {
         let location_href = window.location.href;
-        if(location_href.match(/bilibili.com\/bangumi\/play\/ep/)){
+        if (location_href.match(/bilibili.com\/bangumi\/play\/ep/)) {
             flag_name = 'ep';
             need_vip = window.__INITIAL_STATE__.epInfo.badge === '会员';
             vip_need_pay = window.__INITIAL_STATE__.epPayMent.vipNeedPay;
-        }else if(location_href.match(/bilibili.com\/bangumi\/play\/ss/)){
+        } else if (location_href.match(/bilibili.com\/bangumi\/play\/ss/)) {
             flag_name = 'ss';
             need_vip = window.__INITIAL_STATE__.epInfo.badge === '会员';
             vip_need_pay = window.__INITIAL_STATE__.epPayMent.vipNeedPay;
-        }else if(location_href.match(/bilibili.com\/video\/av/)){
+        } else if (location_href.match(/bilibili.com\/video\/av/)) {
             flag_name = 'av';
             need_vip = false;
             vip_need_pay = false;
-        }else if(location_href.match(/bilibili.com\/video\/BV/)){
+        } else if (location_href.match(/bilibili.com\/video\/BV/)) {
             flag_name = 'bv';
             need_vip = false;
             vip_need_pay = false;
         }
     }
 
-    function get_all_id(){
+    function get_all_id() {
         let _aid, _cid;
-        if(flag_name === 'ep' || flag_name === 'ss'){
+        if (flag_name === 'ep' || flag_name === 'ss') {
             _aid = window.__INITIAL_STATE__.epInfo.aid;
             _cid = window.__INITIAL_STATE__.epInfo.cid;
-        }else if(flag_name === 'av' || flag_name === 'bv') {
+        } else if (flag_name === 'av' || flag_name === 'bv') {
             _aid = window.__INITIAL_STATE__.videoData.aid;
             _cid = window.__INITIAL_STATE__.videoData.cid;
         }
-        return {aid: _aid, cid: _cid}
+        return { aid: _aid, cid: _cid }
     }
 
-    function refresh(){
+    function refresh() {
         console.log('refresh...');
         !!('#video_download')[0] && $('#video_download').hide();
-        if (window.my_dplayer){
+        if (window.my_dplayer) {
             console.log('销毁dplayer');
             window.my_dplayer.destroy();
             window.my_dplayer = null;
@@ -132,55 +172,63 @@
         cid = ids.cid;
     }
 
-    $('body').append('<a id="video_url" style="display:none" target="_blank" referrerpolicy="origin" href="#"></a>');
+    $('body').append('<a id="video_url" style="display:none" target="_blank" href="#"></a>');
+    USE_DASH && $('body').append('<a id="video_url_2" style="display:none" target="_blank" referrerpolicy="origin" href="#"></a>');
+
     // 暂且延迟处理...
-    setTimeout(function(){
+    setTimeout(function () {
         let my_toolbar;
-        if(!!$('#arc_toolbar_report')[0]){
+        if (!!$('#arc_toolbar_report')[0]) {
             my_toolbar =
-                '<div id="arc_toolbar_report_2" class="video-toolbar report-wrap-module report-scroll-module" scrollshow="true"><div class="ops">'+
-                '<span id="bilibili_parse"><i class="van-icon-floatwindow_custome"></i>请求地址</span>'+
-                '<span id="video_download" style="display:none"><i class="van-icon-download"></i>下载视频</span>'+
+                '<div id="arc_toolbar_report_2" class="video-toolbar report-wrap-module report-scroll-module" scrollshow="true"><div class="ops">' +
+                '<span id="bilibili_parse"><i class="van-icon-floatwindow_custome"></i>请求地址</span>' +
+                '<span id="video_download" style="display:none"><i class="van-icon-download"></i>下载视频</span>' +
+                (USE_DASH ? '<span id="video_download_2" style="display:none"><i class="van-icon-download"></i>下载音频</span>' : '') +
                 '</div></div>';
             $("#arc_toolbar_report").after(my_toolbar);
-        }else if(!!$('#toolbar_module')[0]){
+        } else if (!!$('#toolbar_module')[0]) {
             my_toolbar =
-                '<div id="toolbar_module_2" class="tool-bar clearfix report-wrap-module report-scroll-module media-info" scrollshow="true">'+
-                '<div id="bilibili_parse" class="like-info"><i class="iconfont icon-customer-serv"></i><span>请求地址</span></div>'+
-                '<div id="video_download" class="like-info" style="display:none"><i class="iconfont icon-download"></i><span>下载视频</span></div>'+
+                '<div id="toolbar_module_2" class="tool-bar clearfix report-wrap-module report-scroll-module media-info" scrollshow="true">' +
+                '<div id="bilibili_parse" class="like-info"><i class="iconfont icon-customer-serv"></i><span>请求地址</span></div>' +
+                '<div id="video_download" class="like-info" style="display:none"><i class="iconfont icon-download"></i><span>下载视频</span></div>' +
+                (USE_DASH ? '<div id="video_download_2" class="like-info" style="display:none"><i class="iconfont icon-download"></i><span>下载视频</span></div>' : '') +
                 '</div>';
             $("#toolbar_module").after(my_toolbar);
         }
     }, 3000);
 
-    $('body').on('click', '#video_download',function(){
+    $('body').on('click', '#video_download', function () {
         $('#video_url')[0].click();
     });
 
-    $('body').on('click', '#bilibili_parse',function(){
+    $('body').on('click', '#video_download_2', function () {
+        $('#video_url_2')[0].click();
+    });
+
+    $('body').on('click', '#bilibili_parse', function () {
         get_video_status();
 
         // 更新cid和aid - 2
         const ids = get_all_id();
         aid = ids.aid;
         cid = ids.cid;
-        if(!aid){
+        if (!aid) {
             // 异常
             console.log('aid获取出错！');
         }
 
         // 获取视频分页参数q
-        if(flag_name === 'ep' || flag_name === 'ss'){
+        if (flag_name === 'ep' || flag_name === 'ss') {
             p = window.__INITIAL_STATE__.epInfo.i;
-        }else if(flag_name === 'av' || flag_name === 'bv') {
+        } else if (flag_name === 'av' || flag_name === 'bv') {
             p = window.__INITIAL_STATE__.p;
         }
         p = p || 1;
 
         // 获取视频分辨率参数q
-        if(!!$('li.bui-select-item.bui-select-item-active').attr('data-value')){
+        if (!!$('li.bui-select-item.bui-select-item-active').attr('data-value')) {
             q = $('li.bui-select-item.bui-select-item-active').attr('data-value');
-            if(q === '0'){
+            if (q === '0') {
                 let q_max = $('.bui-select-item')[0].dataset.value;
                 q = q_max > 80 ? 80 : q_max;
             }
@@ -188,34 +236,35 @@
         q = q || 80;
 
         // 获取用户状态
-        if(window.__BILI_USER_INFO__){
-            is_login = window.__BILI_USER_INFO__.isLogin;
+        if (window.__BILI_USER_INFO__) {
+            //is_login = window.__BILI_USER_INFO__.isLogin;
             vip_status = window.__BILI_USER_INFO__.vipStatus;
-        }else if(window.__BiliUser__){
-            is_login = window.__BiliUser__.isLogin;
+        } else if (window.__BiliUser__) {
+            //is_login = window.__BiliUser__.isLogin;
             vip_status = window.__BiliUser__.cache.data.vipStatus;
-        }else{
+        } else {
             is_login = false;
             vip_status = 0;
         }
-        if(!is_login || (is_login && vip_status === 0 && need_vip)){
-            if(!!$('.bui-select-item')[0]){
+        if (!is_login || (is_login && vip_status === 0 && need_vip)) {
+            if (!!$('.bui-select-item')[0]) {
                 let q_max = $('.bui-select-item')[0].dataset.value;
                 q = q_max > 80 ? 80 : q_max;
-            }else{
+            } else {
                 q = 80;
             }
             // 暂停视频准备换源
             !!$('video[crossorigin="anonymous"]')[0] && $('video[crossorigin="anonymous"]')[0].pause();
         }
 
-        if (aid === aid_temp && p === p_temp && q === q_temp){
+        if (aid === aid_temp && p === p_temp && q === q_temp) {
             console.log('重复请求');
-            const url = $('#video_url').attr('href')
-            if (url && url !== '#'){
+            const url = $('#video_url').attr('href');
+            const url_2 = USE_DASH ? $('#video_url_2').attr('href') : '#';
+            if (url && url !== '#') {
                 $('#video_download').show();
-                if(!is_login || (is_login && vip_status === 0 && need_vip)){
-                    !$('#my_dplayer')[0] && replace_player(url);
+                if (!is_login || (is_login && vip_status === 0 && need_vip)) {
+                    !$('#my_dplayer')[0] && replace_player(url, url_2);
                 }
             }
             return;
@@ -227,71 +276,97 @@
 
         console.log('开始解析');
         let type, api_url;
-        if(flag_name === 'ep' || flag_name === 'ss'){
+        if (flag_name === 'ep' || flag_name === 'ss') {
             type = 'bangumi';
             epid = window.__INITIAL_STATE__.epInfo.id;
-            api_url = `https://api.injahow.cn/bparse/?av=${aid}&ep=${epid}&q=${q}&otype=url&type=${type}`;
-        }else if(flag_name === 'av' || flag_name === 'bv'){
+            api_url = `https://api.injahow.cn/bparse/?av=${aid}&ep=${epid}&q=${q}&otype=url&type=${type}` + (USE_DASH ? '&dash' : '');
+        } else if (flag_name === 'av' || flag_name === 'bv') {
             type = 'video';
-            api_url = `https://api.injahow.cn/bparse/?av=${aid}&p=${p}&q=${q}&otype=url&type=${type}`;
+            api_url = `https://api.injahow.cn/bparse/?av=${aid}&p=${p}&q=${q}&otype=url&type=${type}` + (USE_DASH ? '&dash' : '');
         }
-        $.ajax({
-            url: api_url,
-            dataType: 'text',
-            success:function(result){
-                if(result !== ''){
-                    console.log('url获取成功');
-                    const url = result.replace(/^https?\:\/\//i, 'https://');
-                    $('#video_url').attr('href', url);
-                    $('#video_download').show();
-                    if(!is_login || (is_login && vip_status === 0 && need_vip)){
-                        replace_player(url);
+        if (USE_DASH) {
+            $.ajax({
+                url: api_url,
+                dataType: 'json',
+                success: function (result) {
+                    if (result.code === 0) {
+                        console.log('url获取成功');
+                        const url = result.video.replace(/^https?\:\/\//i, 'https://');
+                        const url_2 = result.audio.replace(/^https?\:\/\//i, 'https://');
+                        $('#video_url').attr('href', url);
+                        $('#video_url_2').attr('href', url_2);
+                        $('#video_download').show();
+                        USE_DASH && $('#video_download_2').show();
+                        if (!is_login || (is_login && vip_status === 0 && need_vip)) {
+                            replace_player(url, url_2);
+                        }
+                    } else {
+                        console.log('url获取失败');
                     }
-                }else{
-                    console.log('url获取失败');
+                },
+                error: function (error) {
+                    console.log('api请求异常', error);
                 }
-            },
-            error:function(error){
-                console.log('api请求异常', error);
-            }
-        });
+            });
+        } else {
+            $.ajax({
+                url: api_url,
+                dataType: 'text',
+                success: function (result) {
+                    if (result !== '') {
+                        console.log('url获取成功');
+                        const url = result.replace(/^https?\:\/\//i, 'https://');
+                        $('#video_url').attr('href', url);
+                        $('#video_download').show();
+                        if (!is_login || (is_login && vip_status === 0 && need_vip)) {
+                            replace_player(url);
+                        }
+                    } else {
+                        console.log('url获取失败');
+                    }
+                },
+                error: function (error) {
+                    console.log('api请求异常', error);
+                }
+            });
+        }
     });
 
     // 监听p
-    $('body').on('click', 'a.router-link-active', function(){
-        if(this!==$('li[class="on"]').find('a')[0]){
+    $('body').on('click', 'a.router-link-active', function () {
+        if (this !== $('li[class="on"]').find('a')[0]) {
             refresh();
         }
     });
 
-    $('body').on('click', 'li.ep-item', function(){
-        if(!$(this).find('.cursor')){
+    $('body').on('click', 'li.ep-item', function () {
+        if (!$(this).find('.cursor')) {
             refresh();
         }
 
     });
 
     // 监听q
-    $('body').on('click', 'li.bui-select-item', function(){
+    $('body').on('click', 'li.bui-select-item', function () {
         refresh();
     });
 
     // 监听aid 右侧推荐
-    $('body').on('click', '.rec-list', function(){
+    $('body').on('click', '.rec-list', function () {
         refresh();
     });
 
     // 监听aid 视频内部推荐
-    $('body').on('click', '.bilibili-player-ending-panel-box-videos', function(){
+    $('body').on('click', '.bilibili-player-ending-panel-box-videos', function () {
         refresh();
     });
 
     // 定时检查 aid 和 cid
-    setInterval(function(){
+    setInterval(function () {
         const ids = get_all_id();
-        if(aid !== ids.aid || cid !== ids.cid){
+        if (aid !== ids.aid || cid !== ids.cid) {
             refresh();
         }
-    },3000);
+    }, 3000);
 
 })();
